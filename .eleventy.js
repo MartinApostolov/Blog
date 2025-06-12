bd581af18faf1348bfeac30bf1ab0b0701c18931
MartinApostolov <85119661+MartinApostolov@users.noreply.github.com>
2025-06-12 22:59:28 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("scripts.js");
     eleventyConfig.addPassthroughCopy("src/admin");
     eleventyConfig.addPassthroughCopy("src/images");
-    eleventyConfig.addPassthroughCopy("src/videos");
+    eleventyConfig.addPassthroughCopy("src/video");
 
     return {
         dir: {
